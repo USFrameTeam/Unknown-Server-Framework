004d147b8201c56dd43ebfd6d719b0b7caabd1b5
kzyqq00-Player <kzyqq00-Player@outlook.com>
2025-09-27 16:38:18 +0800

    refactor: 使用`JSON.stringify`

--- packer.js
+++ packer.js
@@ -2,27 +2,33 @@ const fs = require('node:fs');
 const archiver = require('archiver');
 const SRC_PATH = 'BehaviourPack/';
 
-function compressionJSON(filename) {
-  return [filename, fs.readFileSync(filename, 'utf8')
-    .replaceAll('\r', '')
-    .replaceAll('\n', '')
-    .replaceAll('\t', '')
-    .replaceAll(' ', '')];
-}
-
-const compressionJSONs = ['entities', 'items', 'cameras/presets'].map(name => {
-  const path = SRC_PATH + name;
-  return fs.readdirSync(path).map(filename => compressionJSON(path + '/' + filename));
-}).flat(1);
-const compressionManifestJSON = compressionJSON(SRC_PATH + 'manifest.json');
-
 const archive = archiver('zip', {
   zlib: { level: 9 }
 });
 
-archive.append(compressionManifestJSON[1], { name: 'manifest.json' });
+archive.append(
+  JSON.stringify(
+    JSON.parse(
+      fs.readFileSync(
+        SRC_PATH + 'manifest.json', 'utf8'
+      )
+    )
+  ),
+  { name: 'manifest.json' }
+);
 archive.file(SRC_PATH + 'pack_icon.png', { name: 'pack_icon.png' });
-compressionJSONs.forEach(data => archive.append(data[1], { name: data[0].substring(SRC_PATH.length) }));
+['entities', 'items', 'cameras/presets'].forEach(name => {
+  const path = SRC_PATH + name;
+
+  fs.readdirSync(path)
+    .map(_filename => ({
+      filename: path + '/' + _filename,
+      content: fs.readFileSync(path + '/' + _filename, 'utf8')
+    }))
+    .forEach(({ filename, content }) =>
+      archive.append(JSON.stringify(JSON.parse(content)), { name: filename.substring(SRC_PATH.length) })
+    );
+});
 archive.directory(SRC_PATH + 'functions', 'functions');
 archive.file('temp/Main.js', { name: 'scripts/Main.js' });
 
@@ -37,8 +43,7 @@ archive.on('error', (err) => {
 
 const output = fs.createWriteStream(name);
 
-output.on('close', () => console.log(`${name} 文件已成功创建，共包含 ${archive.pointer()} 字节`));
-
 archive.pipe(output).on('finish', () => {
   fs.rmSync('temp', { recursive: true, force: true });
+  console.log(`${name} 文件已成功创建，共包含 ${archive.pointer()} 字节`);
 });
